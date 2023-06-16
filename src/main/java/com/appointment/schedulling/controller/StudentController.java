8cf9c51f82e7d213c5998d66bae04517cf8b8642
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-17 01:43:39 +0500

    URLs are assigned to respective method

--- src/main/java/com/appointment/schedulling/controller/StudentController.java
+++ src/main/java/com/appointment/schedulling/controller/StudentController.java
@@ -1,11 +1,38 @@
 package com.appointment.schedulling.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.appointment.schedulling.models.Student;
+import com.appointment.schedulling.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
     //add Student
     //get All Student
     //get by id
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/addStudent")
+    public Student addStudent(@RequestBody Student student){
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping("/allStudents")
+    public List<Student> allStudents(){
+        return studentService.getAllStudent();
+    }
+
+
+    @GetMapping("/{id}")
+    public Optional<Student> findById(Long id){
+        return studentService.findById(id);
+    }
+
+
 
 }
